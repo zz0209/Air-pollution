8ca8721c0371ba6780ef3c30ac6cb4aa6679022c
朱子 <zhuzi@zhuzideMacBook-Pro.local>
2020-09-16 20:29:35 +0800

    finished reading the file

--- file reading module.R
+++ file reading module.R
@@ -1,17 +1,25 @@
-#reading the files from the directory "城市_20150101-20141231"
+#reading the files from the directory "城市_20160101-20161231"
 options(max.print=1000000)
 
 ##generating a vector of directories
 dict_str <- vector()
 
 for (i in 1:(length(name_str))){
-  dict_str[i] <- paste("~/Desktop/课外活动/科研项目/论文项目/数据/PM2.5浓度数据/全国空气质量/城市_20150101-20141231/", name_str[i], sep = "")
+  dict_str[i] <- paste("~/Desktop/课外活动/科研项目/论文项目/数据/PM2.5浓度数据/全国空气质量/城市_20160101-20161231/", name_str[i], sep = "")
 }
 
 dict_str
 
+##reading and recording the data
+dataframes <- list()
 
-a <- read.csv("~/Desktop/课外活动/科研项目/论文项目/数据/PM2.5浓度数据/全国空气质量/城市_20150101-20141231/china_cities_20150102.csv")
-a
+for (i in 1:(length(name_str))){
+  if (!dict_str[i] == "~/Desktop/课外活动/科研项目/论文项目/数据/PM2.5浓度数据/全国空气质量/城市_20160101-20161231/china_cities_20160230.csv"){
+    dataframes[i] <- list(read.csv(dict_str[i]))[1]
+  }
+}
 
-setwd("/Lexar/研究空气污染/论文项目/PM2.5浓度数据/全国空气质量/城市_20150101-20141231")
+class(dataframes[[1]])
+dataframes[[320]]
+
+#
